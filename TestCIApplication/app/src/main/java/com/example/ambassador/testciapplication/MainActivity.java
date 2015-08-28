f2609ff6877a825e0484451160ee3d685a338898
jakedunahee <jake@getambassador.com>
2015-08-28 11:55:37 -0400

    Added another prinln

--- TestCIApplication/app/src/main/java/com/example/ambassador/testciapplication/MainActivity.java
+++ TestCIApplication/app/src/main/java/com/example/ambassador/testciapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Successfully created");
+        System.out.println("Another change");
     }
 
     @Override
